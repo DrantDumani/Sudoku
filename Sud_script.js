452a70d076e9333cb39d813d868ece0ac1c5fb41
DrantDumani <darnjames@gmail.com>
2020-03-06 13:25:48 -0500

    Restarting Daily work on sudoku project

--- Sud_script.js
+++ Sud_script.js
@@ -1,7 +1,22 @@
 let gameBrd = document.querySelector(".game-board")
 
-for (let space = 0; space < 81; space++) {
-	let input = document.createElement("input")
-	input.classList.add("game-tile")
-	gameBrd.appendChild(input)
+for (let row = 1; row <= 9; row++) {
+	let trow = document.createElement("tr")
+	trow.classList.add("table-row")
+	for (let cell = 1; cell <= 9; cell++) {
+		let tcell = document.createElement("td")
+		let input = document.createElement("input")
+		input.setAttribute("maxlength", 1)
+		input.classList.add("num-space")
+		if ((row == 3) || (row == 6)) {
+			tcell.classList.add("section-divider-V")
+		}
+		if ((cell == 3) || (cell == 6)) {
+			tcell.classList.add("section-divider-H")
+		} 
+		tcell.appendChild(input)
+		tcell.classList.add("game-tile")
+		trow.appendChild(tcell)
+	}
+	gameBrd.appendChild(trow)
 }
